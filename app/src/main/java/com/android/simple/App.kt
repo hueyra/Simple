093b4531abfce4fb225c92ac14e47134e64f9d86
zhujun <hueyra@163.com>
2022-03-28 18:18:24 +0800

    first commit

---
+++ app/src/main/java/com/android/simple/App.kt
@@ -0,0 +1,18 @@
+package com.android.simple
+
+import android.app.Application
+
+/**
+ * Created by zhujun
+ * Date : 2022-03-25
+ * Desc : _
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+}
